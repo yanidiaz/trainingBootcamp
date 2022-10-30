b0142b45cf4b4ccfeb4276edcd920bf60b0cf90c
yanidiaz <yanira.diazcruz@gmail.com>
2022-10-30 12:48:08 -0600

    added from intelliJ

---
+++ trainingBootcampFinal/src/main/java/Client.java
@@ -0,0 +1,14 @@
+public class Client {
+    public static  void main (String []pruebayani){
+
+        int a = 9;
+        int b= 8;
+        if (a> b){
+            a=a*2;
+            System.out.println(a);
+        }else {
+            b=b-2;
+            System.out.println(b);
+        }
+    }
+}
